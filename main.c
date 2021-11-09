22fe271e5cacfedcf5d7570a5095538d7f892985
Mehdi <pray5times.project@gmail.com>
2021-11-10 00:57:25 +0100

    Add option to calculate prayers times for a specific date

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 #include "prayer_time.h"
 
-const char *usage= "Usage: prayer_time -h -l [latitude] -L [longitude]";
+const char *usage= "Usage: prayer_time -h -l [latitude] -L [longitude] -d [date]
 
 static double convert_gregorian_to_julian(struct tm *tm)
 {
@@ -28,17 +28,21 @@ static int calculate_time(struct prayer_struct *prayer_struct, struct calc_param
 	long int timezone;
 	struct tm *tm;
 
-	tm = localtime(&t);
+	if (prayer_struct->tm_custom.tm_year != 0) {
+		param->julian_date = convert_gregorian_to_julian(&(prayer_struct->tm_custom));
+	} else {
+		tm = localtime(&t);
 
-	timezone = second_to_hour(tm->tm_gmtoff);
+		timezone = second_to_hour(tm->tm_gmtoff);
 
-	log_info("Date: %d-%02d-%02d\n", tm->tm_year, tm->tm_mon + 1, tm->tm_mday);
+		log_info("Date: %d-%02d-%02d\n", tm->tm_year, tm->tm_mon + 1, tm->tm_mday);
 
-	log_info("Timezone : %ld\n", timezone);
+		log_info("Timezone : %ld\n", timezone);
 
-	param->julian_date = convert_gregorian_to_julian(tm);
+		param->julian_date = convert_gregorian_to_julian(tm);
 
-	log_info("Julian_Date: %lf\n", param->julian_date);
+		log_info("Julian_Date: %lf\n", param->julian_date);
+	}
 
 	compute_equation_of_time(&equa_of_time, param);
 
@@ -61,11 +65,44 @@ int main(int argc, char **argv)
 {
 	int c;
 	int rc;
+	int i;
+	char *sep = "-";
+	char *res;
+	char *date[3];
 	struct prayer_struct prayer_struct = {0};
 	struct calc_param *param;
 
-	while((c = getopt(argc, argv, "hl:L:")) != -1) {
+	while((c = getopt(argc, argv, "d:hl:L:")) != -1) {
 		switch(c) {
+			case 'd':
+				for (i = 0; i < 3 ; i++) {
+					date[i] = malloc(4);
+
+					if (date[i] == NULL) {
+						log_error("Memory allocation failed\n");
+						goto free_error;
+					}
+				}
+
+				res = strtok(optarg, sep);
+				memcpy(date[0], res, strlen(res));
+
+				i = 1;
+				while (res != NULL) {
+					res = strtok(NULL, sep);
+					if (res != NULL)
+						memcpy(date[i], res, strlen(res));
+					i++;
+				};
+
+				prayer_struct.tm_custom.tm_mday = atoi(date[0]);
+				prayer_struct.tm_custom.tm_mon = atoi(date[1]);
+				prayer_struct.tm_custom.tm_year = atoi(date[2]);
+
+				for (i = 0; i < 3; i++) {
+					free(date[i]);
+				}
+				break;
 			case 'l':
 				prayer_struct.latitude = atof(optarg);
 				log_info("Latitude: %f\n", prayer_struct.latitude);
@@ -101,4 +138,12 @@ int main(int argc, char **argv)
 	free(param);
 
 	return 0;
+
+free_error:
+	for (i = 0; i < 3; i++) {
+		if (date[i] != NULL)
+			free(date[i]);
+	}
+
+	return 1;
 }
