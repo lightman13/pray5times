145b7a55712d5dac8194ed22b0d90c69ef8c75f7
Mehdi <pray5times.project@gmail.com>
2022-03-02 23:00:57 +0100

    tests: Add tap output file

--- src/tests/tests_suite.c
+++ src/tests/tests_suite.c
@@ -3,12 +3,14 @@
 int main(void)
 {
 	int no_failed = 0;
+	const char *filename = "results.tap";
 
 	Suite *s;
 	SRunner *runner;
 
 	s = utils_suite();
 	runner = srunner_create(s);
+	srunner_set_tap(runner, filename);
 	srunner_add_suite(runner, date_suite());
 	srunner_add_suite(runner, sun_position_suite());
 	srunner_add_suite(runner, cal_prayer_suite());
